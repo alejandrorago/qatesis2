28d33c2b21b9f201c0a7c7914415012f46839915
franciscobertoni <franciscobertoni@6c9b6502-2b84-11df-a4b8-f77f9512071c>
2010-04-07 18:14:53 +0000

    Cambios

--- proyecto/src/algorithms/OntologyAlgorithm.java
+++ proyecto/src/algorithms/OntologyAlgorithm.java
@@ -5,8 +5,14 @@ import entities.QualityAttributeInterface;
 import entities.QualityAttributeTheme;
 import entities.RichedWord;
 
+import ontology.OntologyAnalyzer;
 import ontology.QualityAttributeBelongable;
 
+import org.apache.log4j.Logger;
+
+import utils.MapUtils;
+
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -17,82 +23,83 @@ import java.util.Map;
  *
  * @author $author$
  * @version $Revision$
-  */
+ */
 public class OntologyAlgorithm implements Algorithm {
     /**
      * DOCUMENT ME!
      */
+    static final Logger logger = Logger.getLogger(OntologyAlgorithm.class);
+
+    /** DOCUMENT ME! */
     private QualityAttributeBelongable qabelongable;
 
     /**
-     * Devuelve un QualityAttributeTheme a partir de una lista de palabras y de un EarlyAspect
+     * Creates a new OntologyAlgorithm object.
+     *
+     * @param owlFilePath DOCUMENT ME!
+     * @param repositoryFilePath DOCUMENT ME!
+     */
+    public OntologyAlgorithm(String owlFilePath, String repositoryFilePath) {
+        this.qabelongable = new OntologyAnalyzer(owlFilePath, repositoryFilePath);
+    }
+
+    /**
+     * Devuelve un QualityAttributeTheme a partir de una lista de
+     * palabras y de un EarlyAspect
      *
      * @param words DOCUMENT ME!
      * @param earlyAspect DOCUMENT ME!
      *
      * @return DOCUMENT ME!
      */
+
     //TODO darle bolilla al EarlyAspect, ¿como lo usamos?
     @Override
     public QualityAttributeTheme getQualityAttributeTheme(
-       
-    	List<RichedWord> words, EarlyAspect earlyAspect) {
+        List<RichedWord> words, EarlyAspect earlyAspect) {
+        Map<QualityAttributeInterface, Double> map = this.getAttributesMap(words);
+        MapUtils.imprimirMap(map);
+
+        return null;
+    }
+
+    /**
+     * Metodo que devuelve el grado de pertenencia de una lista de
+     * palabras, con los distintos atributos de calidad. En el map que
+     * devuelve, cada clave es un atributo de calidad y el valor de la clave
+     * un numero entre 0 y 1 indicando el grado de pertenecia de la lista con
+     * ese atributo.
+     *
+     * @param words listado de palabras
+     *
+     * @return map en donde cada clave es un atributo de calidad, y el valor de
+     *         la clava un numero entre 0 y 1 indicando el grado de pertenecia
+     *         de la lista con ese atributo.
+     */
+    private Map<QualityAttributeInterface, Double> getAttributesMap(
+        List<RichedWord> words) {
         Map<QualityAttributeInterface, Double> wordMap = null;
         Map<QualityAttributeInterface, Double> totalMap = qabelongable.loadQualityAttributes();
 
         Iterator<RichedWord> richedWordIterator = words.iterator();
         String word = null;
+        Integer totalWords = 0;
 
         while (richedWordIterator.hasNext()) {
             word = richedWordIterator.next().getWord();
-            wordMap = qabelongable.getWordPertenence(word);
-            totalMap = addTotal(totalMap, wordMap);
+            wordMap = MapUtils.convertMapToPromedy(qabelongable.getWordPertenence(
+                        word));
+
+            if (wordMap != null) {
+                totalMap = MapUtils.addTotal(totalMap, wordMap);
+                totalWords++;
+            }
         }
 
-        
-        return null;
-    }
-
-    /**
-     * Suma a cada valor de cada atributo de calidad de totalMap, el valor que se encuentra en el mismo atributo del map wordmap
-     *
-     * @param totalMap map al que se le suma el valor
-     * @param wordMap map de donde se saca el valor de cada clave, y se le suma al valor de la misma clave perteneciente a totalMap
-     *
-     * @return map con los valores sumados de ambos maps
-     */
-    private  Map<QualityAttributeInterface, Double> addTotal(
-        Map<QualityAttributeInterface, Double> totalMap,
-        Map<QualityAttributeInterface, Double> wordMap) {
-        Iterator<QualityAttributeInterface> qaIterator = wordMap.keySet()
-                                                                .iterator();
-        QualityAttributeInterface qaInterface = null;
-                
-        while (qaIterator.hasNext()) {
-            qaInterface = qaIterator.next();
-            totalMap.put(qaInterface,
-                totalMap.get(qaInterface) + wordMap.get(qaInterface));
-        }
+        totalMap = MapUtils.divideTotal(totalMap, totalWords);
 
         return totalMap;
     }
-    
-    private  Map<QualityAttributeInterface, Double> divideTotal(
-            Map<QualityAttributeInterface, Double> totalMap,
-            Double totalWords) {
-            
-    		Iterator<QualityAttributeInterface> qaIterator = totalMap.keySet()
-                                                                    .iterator();
-            QualityAttributeInterface qaInterface = null;
-
-            while (qaIterator.hasNext()) {
-                qaInterface = qaIterator.next();
-                totalMap.put(qaInterface,
-                    totalMap.get(qaInterface)/Integer.valueOf(2));
-            }
-
-            return totalMap;
-        }
 
     /**
      * Devuelve el QualityAttributeBelongable
@@ -110,5 +117,21 @@ public class OntologyAlgorithm implements Algorithm {
      */
     public void setQabelongable(QualityAttributeBelongable qabelongable) {
         this.qabelongable = qabelongable;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param args DOCUMENT ME!
+     */
+    public static void main(String[] args) {
+        Algorithm algorithm = new OntologyAlgorithm("file:resources/ontology.repository",
+                "file:resources/ontology.owl");
+
+        List<RichedWord> list = new ArrayList<RichedWord>();
+        list.add(new RichedWord("User2"));
+        list.add(new RichedWord("Stimulus1"));
+
+        QualityAttributeTheme qt = algorithm.getQualityAttributeTheme(list, null);
     }
 }
