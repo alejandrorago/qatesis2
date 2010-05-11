dc535e5dd330cd021587800b29567691e94ae9ef
franciscobertoni <franciscobertoni@6c9b6502-2b84-11df-a4b8-f77f9512071c>
2010-05-11 13:18:40 +0000

    Se vuelve a agregar el Early Aspect

--- proyecto/src/entities/AbstractEntityFactory.java
+++ proyecto/src/entities/AbstractEntityFactory.java
@@ -3,48 +3,11 @@ package entities;
 import java.util.List;
 import java.util.Map;
 
-
-/**
- * DOCUMENT ME!
- *
- * @author $author$
- * @version $Revision$
-  */
 public abstract class AbstractEntityFactory {
-    /**
-     * DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
-     */
-    public abstract UseCaseInterface createUseCase();
 
-    /**
-     * DOCUMENT ME!
-     *
-     * @param name DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
-     */
-    public abstract QualityAttributeInterface creatQualityAttribute(String name);
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param useCases DOCUMENT ME!
-     * @param map DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
-     */
-    public abstract QualityAttributeThemeInterface creatQualityAttributeTheme(
-        List<UseCaseInterface> useCases,
-        Map<QualityAttributeInterface, Double> map);
-
-    /**
-     * DOCUMENT ME!
-     *
-     * @param word DOCUMENT ME!
-     *
-     * @return DOCUMENT ME!
-     */
-    public abstract RichedWordInterface creatRichedWord(String word);
+	public abstract UseCaseInterface createUseCase();
+	public abstract EarlyAspectInterface creatEarlyAspect(String verb, String noun);
+	public abstract QualityAttributeInterface creatQualityAttribute(String name);
+	public abstract QualityAttributeThemeInterface creatQualityAttributeTheme(List<UseCaseInterface> useCases, EarlyAspectInterface earlyAspect, Map<QualityAttributeInterface, Double> map);
+	public abstract RichedWordInterface creatRichedWord(String word);
 }
