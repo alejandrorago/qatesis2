5a9d46884fd872064fb2403c94438c3f3e54092e
franciscobertoni <franciscobertoni@6c9b6502-2b84-11df-a4b8-f77f9512071c>
2010-05-16 21:41:15 +0000

    Se cambia el metodo loadQualityAttributes, para que en vez de un map devuelva una lista con los atributos de calidad de la ontologia.

--- proyecto/src/ontology/QualityAttributeBelongable.java
+++ proyecto/src/ontology/QualityAttributeBelongable.java
@@ -1,11 +1,12 @@
 package ontology;
 
+import java.util.List;
 import java.util.Map;
 
 import entities.QualityAttributeInterface;
 
 public interface QualityAttributeBelongable {
 	Map<QualityAttributeInterface,Double> getWordPertenence(String word);
-    Map<QualityAttributeInterface, Double> loadQualityAttributes();
+    List<QualityAttributeInterface> loadQualityAttributes();
 
 }
